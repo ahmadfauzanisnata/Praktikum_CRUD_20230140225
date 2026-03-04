7c92d13ea114f4e2f92e75c93428cd48b9184704
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2026-03-04 09:58:37 +0700

    implement full user management logic

--- src/main/java/com/deploy/praktikum1/service/lmpl/UserServidelmpl.java
+++ src/main/java/com/deploy/praktikum1/service/lmpl/UserServidelmpl.java
@@ -40,3 +40,42 @@ public class UserServiceImpl implements UserService {
         return userDto;
     }
 }
+@Override
+public List<UserDto> getAllUser() {
+    List<User> users = userRepository.findAll();
+    List<UserDto> userDto = new ArrayList<>();
+    for (User user : users) {
+        userDto.add(UserMapper.MAPPER.toUserDtoData(user));
+    }
+    return userDto;
+}
+
+@Override
+public UserDto getUserById(String id) {
+    User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("user not found"));
+    return UserMapper.MAPPER.toUserDtoData(user);
+}
+
+@Override
+public UserDto UpdateUser(String id, UserAddRequest request) {
+    validationUtil.validate(request);
+
+    User existingUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("user not found"));
+
+    User user = User.builder()
+            .id(existingUser.getId())
+            .name(request.getName())
+            .age(request.getAge())
+            .build();
+
+    userRepository.save(user);
+
+    return UserMapper.MAPPER.toUserDtoData(user);
+}
+
+@Override
+public void DeleteUser(String id) {
+    User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("user not found"));
+    userRepository.delete(user);
+}
+}
